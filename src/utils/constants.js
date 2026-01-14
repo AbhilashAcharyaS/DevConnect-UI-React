ee21b7de3383c4493058fefc553f3ea475f07d72
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2026-01-14 13:44:18 +0530

    update config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@
 //  export const BASE_URL="http://localhost:3000";
 
 export const BASE_URL=
-location.hostname === "localhost" ? "http://localhost:3000" :"/api";
+location.hostname === "localhost" ? "http://localhost:3000" :"https://dev-connect-seven-woad.vercel.app";
