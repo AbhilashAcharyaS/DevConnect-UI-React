8936652cb42b5f860d7e7604eb01eebaca53cb30
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2026-05-05 23:29:29 +0530

    update backend live url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@
 //  export const BASE_URL="http://localhost:3000";
 
 export const BASE_URL=
-location.hostname === "localhost" ? "http://localhost:3000" :"https://dev-connect-seven-woad.vercel.app";
+location.hostname === "localhost" ? "http://localhost:3000" :"https://devconnect-backend-lpy6.onrender.com";
