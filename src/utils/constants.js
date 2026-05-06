ba2d6022635a517fd9956f6baceff0bdeb953bcc
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2026-05-06 14:21:10 +0530

    update socket connection

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,5 +4,7 @@
 //dev
 //  export const BASE_URL="http://localhost:3000";
 
-export const BASE_URL=
-location.hostname === "localhost" ? "http://localhost:3000" :"https://devconnect-backend-lpy6.onrender.com";
+// export const BASE_URL=
+// location.hostname === "localhost" ? "http://localhost:3000" :"https://devconnect-backend-lpy6.onrender.com";
+
+export const BASE_URL = "https://devconnect-backend-lpy6.onrender.com";
